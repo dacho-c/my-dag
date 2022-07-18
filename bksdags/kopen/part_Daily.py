299b35d4f83b2a626c6e0657bd33649e08470ca0
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2022-07-18 11:08:59 +0700

    Update 18 July 2022#6

--- bksdags/kopen/part_Daily.py
+++ bksdags/kopen/part_Daily.py
@@ -7,7 +7,7 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 import sys, os
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 from Class import common
-
+self = []
 with DAG(
     dag_id='Kopen_Part_Daily_db2postgres_dag',
     schedule_interval='0 5 * * *',
@@ -20,7 +20,7 @@ with DAG(
     task_EL_Kopen_Part_data = PythonOperator(
         task_id='el_kopen_part_data',
         provide_context=True,
-        python_callable=common.read_load_save_data('',"product", "kp_part", 50000),
+        python_callable=common.read_load_save_data(self,"product", "kp_part", 50000),
         #op_kwargs={'From_Table': "product", 'To_Table': "kp_part", 'Chunk_Size': 50000}
     )
 

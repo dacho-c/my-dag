ca252034b1ed1589b932870c2a6a3de8f2214630
dacho.chuensiri <dacho.chuensiri@gmail.com>
2022-07-22 18:08:02 +0700

    Update 22 July 2022#1

--- bksdags/datawarehouse/machine.py
+++ bksdags/datawarehouse/machine.py
@@ -11,7 +11,7 @@ from Class import common
 from function import get_last_ym
 
 with DAG(
-    dag_id='DWH_ETL_SalesByItem_db2postgres_dag',
+    dag_id='DWH_ETL_Machine_db2postgres_dag',
     schedule_interval='25 7-20/1 * * *',
     #start_date=datetime(year=2022, month=6, day=1),
     start_date=pendulum.datetime(2022, 6, 1, tz="Asia/Bangkok"),
@@ -19,11 +19,11 @@ with DAG(
 ) as dag:
 
     # 1. Generate Sales By Item from a DATA LAKE To DATA Warehouse
-    task_ETL_WH_SalesByItem = PythonOperator(
-        task_id='etl_salesbyitem_data',
+    task_ETL_WH_Machine = PythonOperator(
+        task_id='etl_machine_data',
         provide_context=True,
         python_callable=common.read_load_update_data,
-        op_kwargs={'From_Table': "sales_by_item", 'To_Table': "sales_by_item", 'Chunk_Size': 50000, 'Condition': "smm_account_month >= '%s'" % (get_last_ym())}
+        op_kwargs={'From_Table': "machine", 'To_Table': "machine", 'Chunk_Size': 50000, 'Condition': "smm_account_month >= '%s'" % (get_last_ym())}
     )
 
-    task_ETL_WH_SalesByItem
+    task_ETL_WH_Machine

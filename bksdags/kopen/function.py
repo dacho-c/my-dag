f75794368b964dfbb6353bbe64000d4cac715944
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2022-09-27 16:51:00 +0700

    Update 27 Sep 2022#5

--- bksdags/kopen/function.py
+++ bksdags/kopen/function.py
@@ -55,3 +55,14 @@ def get_first_ym_fisical_year():
         today = today.replace(year=y)
         today = today.replace(month=4)
     return today.strftime("%Y%m")
+
+def get_Last_fisical_year():
+    today = datetime.date.today()
+    m = today.month
+    if (m >= 4):
+        today = today.replace(month=4)
+    else:
+        y = today.year - 1
+        today = today.replace(year=y)
+        today = today.replace(month=4)
+    return today.strftime("%Y%m")

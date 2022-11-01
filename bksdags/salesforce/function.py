79e7309bd5c924e371d952397c202e82c06ce433
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2022-11-01 09:31:39 +0700

    Update 1 Nov 2022#1

---
+++ bksdags/salesforce/function.py
@@ -0,0 +1,57 @@
+import datetime
+import pandas as pd
+
+def get_last_ym():
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    lastMonth = first - datetime.timedelta(days=1)
+    return lastMonth.strftime("%Y%m")
+
+def get_this_ym():
+    today = datetime.date.today()
+    return today.strftime("%Y%m")
+
+def get_today():
+    today = datetime.date.today()
+    return today.strftime("%Y-%m-%d")
+
+def get_yesterday():
+    today = datetime.date.today()
+    yday = today - datetime.timedelta(days=1)
+    return yday.strftime("%Y-%m-%d")
+
+def get_firstdate_this_m():
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    return first.strftime("%Y-%m-%d")
+
+def get_lastdate_this_m():
+    today = datetime.date.today()
+    p = pd.Period(today.strftime("%Y-%m-%d"))
+    lday = p.days_in_month
+    last = today.replace(day=lday)
+    return last.strftime("%Y-%m-%d")
+
+def get_firstdate_last_m():
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    lastMonth = first - datetime.timedelta(days=1)
+    first = lastMonth.replace(day=1)
+    return first.strftime("%Y-%m-%d")
+
+def get_lastdate_last_m():
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    lastMonth = first - datetime.timedelta(days=1)
+    return lastMonth.strftime("%Y-%m-%d")
+
+def get_first_ym_fisical_year():
+    today = datetime.date.today()
+    m = today.month
+    if (m >= 4):
+        today = today.replace(month=4)
+    else:
+        y = today.year - 1
+        today = today.replace(year=y)
+        today = today.replace(month=4)
+    return today.strftime("%Y%m")
---
+++ bksdags/salesforce/opportunity.py
@@ -0,0 +1,215 @@
+from datetime import datetime, timedelta
+import pendulum
+from airflow.models import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.dummy import DummyOperator 
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.utils.edgemodifier import Label
+from airflow.utils.trigger_rule import TriggerRule
+
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import Column, Integer, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData, Table
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.inspection import inspect
+import sys, os
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+#from function import get_yesterday
+from Class import common
+from function import get_firstdate_last_m, get_today
+
+def upsert(session, table, update_cols, rows):
+
+    stmt = insert(table).values(rows)
+
+    on_conflict_stmt = stmt.on_conflict_do_update(
+        index_elements=table.primary_key.columns,
+        set_={k: getattr(stmt.excluded, k) for k in update_cols}
+        #,
+        #index_where= ( as_of_date_col < getattr(stmt.excluded, as_of_date_col))
+        )
+    session.execute(on_conflict_stmt)
+
+def UPSERT_process(**kwargs):
+    
+    dlstrcon = common.get_pg_connection('')
+    # Create SQLAlchemy engine
+    engine = sqlalchemy.create_engine(dlstrcon,client_encoding="utf8")
+    # Start Session
+    Base = declarative_base()
+    session = sessionmaker(bind=engine)
+    session = session()
+    Base.metadata.create_all(engine)
+
+    n = 0
+    rows = 0
+    tb_to = kwargs['To_Table']
+    schema = 'public'
+    no_update_cols = []
+
+    print('Initial state:\n')
+    df_main = pd.read_sql_query(sql=sqlalchemy.text("select * from %s_tmp" % (tb_to)), con=engine)
+    rows = len(df_main)
+    if (rows > 0):
+        metadata = MetaData(schema=schema)
+        metadata.bind = engine
+        table = Table(tb_to, metadata, schema=schema, autoload=True)
+        update_cols = [c.name for c in table.c
+            if c not in list(table.primary_key.columns)
+            and c.name not in no_update_cols]
+
+        for index, row in df_main.iterrows():
+            print(f"Upsert progress {index + 1}/{rows}")
+            upsert(session,table,update_cols,row)
+    
+    print(f"Upsert Completed {rows} records.\n")
+    session.commit()
+    session.close()
+    print('Upsert session commit')
+
+def INSERT_bluk(**kwargs):
+    
+    dlstrcon = common.get_pg_connection('')
+    # Create SQLAlchemy engine
+    engine = sqlalchemy.create_engine(dlstrcon,client_encoding="utf8")
+
+    tb_to = kwargs['To_Table']
+    primary_key = kwargs['Key']
+    strexec = ("ALTER TABLE IF EXISTS %s RENAME TO %s;" % (tb_to + '_tmp', tb_to))
+    strexec += ("ALTER TABLE %s ADD PRIMARY KEY (%s);" % (tb_to, primary_key))
+    # execute
+    with engine.connect() as conn:
+        conn.execute(strexec)
+        print("ETL DL Process finished")
+        conn.close()
+
+def Cleansing_process(**kwargs):
+    
+    dlstrcon = common.get_pg_connection('')
+    # Create SQLAlchemy engine
+    engine = sqlalchemy.create_engine(dlstrcon,client_encoding="utf8")
+
+    tb_to = kwargs['To_Table']
+    primary_key = kwargs['Key']
+    C_condition = kwargs['Condition']
+
+    # check exiting table
+    ctable = "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = '%s');" % (tb_to + '_tmp')
+    result = pd.read_sql_query(sql=sqlalchemy.text(ctable), con=engine)
+    if result.loc[0,'exists']:
+        strexec = ("DELETE FROM %s WHERE NOT (EXISTS (SELECT %s FROM %s WHERE %s.%s = %s.%s)) %s;") % (tb_to, primary_key, tb_to + '_tmp',tb_to,primary_key,tb_to + '_tmp',primary_key,C_condition)
+        # execute
+        with engine.connect() as conn:
+            conn.execute(strexec)
+            print("Drop Temp Table.")
+            conn.execute("DROP TABLE IF EXISTS %s;" % (tb_to + '_tmp'))
+            conn.close()
+
+def Check_exiting(**kwargs):
+    
+    dlstrcon = common.get_pg_connection('')
+    # Create SQLAlchemy engine
+    engine = sqlalchemy.create_engine(dlstrcon,client_encoding="utf8")
+
+    tb_to = kwargs['To_Table']
+
+    # check exiting table
+    ctable = "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = '%s');" % (tb_to)
+    result = pd.read_sql_query(sql=sqlalchemy.text(ctable), con=engine)
+    if result.loc[0,'exists']:
+        return True
+    else:
+        return False   
+
+def branch_func(ti):
+    xcom_value = bool(ti.xcom_pull(task_ids="check_exit_sf_opportunity_data", key='return_value'))
+    if xcom_value:
+        return "upsert_sf_opportunity_on_data_lake"
+    else:
+        return "create_new_sf_opportunity_table"
+
+default_args = {'start_date': pendulum.datetime(2022, 6, 1, tz="Asia/Bangkok"),
+                'retries': 1,
+                'retry_delay': timedelta(minutes=10),
+                'email': ['dacho-c@bangkokkomatsusales.com'],
+                'email_on_failure': True}
+
+with DAG(
+    dag_id='Salesforce_opportunity_ETL_dag',
+    tags=['Salesforce'],
+    schedule_interval='21 6-18/4 * * *',
+    #start_date=datetime(year=2022, month=6, day=1),
+    default_args=default_args,
+    catchup=False
+) as dag:
+
+    # 1. Check if the API is up
+    task_is_api_active = HttpSensor(
+        task_id='is_api_active',
+        http_conn_id='bks_api',
+        endpoint='genreport/',
+        execution_timeout=timedelta(seconds=120),
+        timeout=3600,
+        retries=3,
+        mode="reschedule",
+    )
+    #data={"stdate": get_firstdate_last_m(), "edate": get_today()},
+    # 2. Call API Salesforce Load to Datalaken Temp Table
+    task_Get_Salesforce_Data_Save_To_Datalake = SimpleHttpOperator(
+        task_id='get_salesforce_opportunity_object',
+        http_conn_id='bks_api',
+        method='GET',
+        endpoint='etl/sf/sfopportunity',
+        data={"stdate": '2022-02-01', "edate": '2022-05-31'},
+        log_response=True
+    )
+
+    # 3. Upsert Salesforce To DATA Lake
+    task_L_DL_SF_opportunity = PythonOperator(
+        task_id='upsert_sf_opportunity_on_data_lake',
+        provide_context=True,
+        python_callable= UPSERT_process,
+        op_kwargs={'To_Table': "sf_opportunity", 'Chunk_Size': 5000, 'Key': 'id', 'Condition': ""}
+    )
+
+    # 4. Replace Salesforce Temp Table
+    task_RP_DL_SF_opportunity = PythonOperator(
+        task_id='create_new_sf_opportunity_table',
+        provide_context=True,
+        python_callable= INSERT_bluk,
+        op_kwargs={'To_Table': "sf_opportunity", 'Chunk_Size': 5000, 'Key': 'id', 'Condition': ""}
+    )
+
+    # 5. Cleansing Salesforce Table
+    task_CL_DL_SF_opportunity = PythonOperator(
+        task_id='cleansing_sf_opportunity_data',
+        provide_context=True,
+        python_callable= Cleansing_process,
+        op_kwargs={'To_Table': "sf_opportunity", 'Chunk_Size': 5000, 'Key': 'id', 'Condition': ""}
+    )
+
+    # 6. Check Exiting Salesforce Table
+    task_CK_DL_SF_opportunity = PythonOperator(
+        task_id='check_exit_sf_opportunity_data',
+        provide_context=True,
+        python_callable= Check_exiting,
+        op_kwargs={'To_Table': "sf_opportunity", 'Chunk_Size': 5000, 'Key': 'id', 'Condition': ""}
+    )
+
+    branch_op = BranchPythonOperator(
+        task_id="check_existing_sf_opportunity_on_data_lake",
+        python_callable=branch_func,
+    )
+
+    branch_join = DummyOperator(
+        task_id='join',
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
+    )
+
+    task_is_api_active >> task_Get_Salesforce_Data_Save_To_Datalake >> task_CK_DL_SF_opportunity >> branch_op >> [task_L_DL_SF_opportunity, task_RP_DL_SF_opportunity] >> branch_join >> task_CL_DL_SF_opportunity

a5a8fc7d1922ea14ac021b1f6b74d9f0f029ebb4
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2022-11-01 12:59:41 +0700

    Update 1 Nov 2022#8

--- bksdags/salesforce/opportunity.py
+++ bksdags/salesforce/opportunity.py
@@ -166,7 +166,7 @@ with DAG(
         http_conn_id='bks_api',
         method='GET',
         endpoint='etl/sf/sfopportunity',
-        data={"stdate": '2022-06-01', "edate": '2022-11-30'},
+        data={"stdate": '2022-06-01', "edate": '2022-07-31'},
         log_response=True
     )
 

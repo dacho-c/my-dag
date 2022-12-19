4e89fe8d91c59bd379b7627d3ffc4328876fa7a2
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2022-12-19 16:00:33 +0700

    Update 19 Dec 2022#1

--- bksdags/kopen/Kopen_Main_Daily.py
+++ bksdags/kopen/Kopen_Main_Daily.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pendulum
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator 
@@ -7,6 +7,7 @@ from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.edgemodifier import Label
 from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.dates import days_ago
 
 import pandas as pd
 import sqlalchemy
@@ -127,9 +128,10 @@ def INSERT_bluk(**kwargs):
     engine = sqlalchemy.create_engine(dlstrcon,client_encoding="utf8")
 
     tb_to = kwargs['To_Table']
+    primary_key = kwargs['Key']
 
     strexec = ("ALTER TABLE IF EXISTS %s RENAME TO %s;" % (tb_to + '_tmp', tb_to))
-    strexec += ("ALTER TABLE %s ADD PRIMARY KEY (pro_komcode);" % (tb_to))
+    strexec += ("ALTER TABLE %s ADD PRIMARY KEY (%s);" % (tb_to, primary_key))
     # execute
     with engine.connect() as conn:
         conn.execute(strexec)
@@ -158,22 +160,141 @@ def Cleansing_process(**kwargs):
             conn.execute("DROP TABLE IF EXISTS %s;" % (tb_to + '_tmp'))
             conn.close()
         
-def branch_func(ti):
+def branch_part_func(ti):
     xcom_value = bool(ti.xcom_pull(task_ids="etl_kopen_part_data", key='return_value'))
     if xcom_value:
         return "upsert_part_on_data_warehouse"
     else:
         return "create_new_part_table"
 
+args = {
+        'owner': 'airflow',    
+        #'start_date': airflow.utils.dates.days_ago(2),
+        # 'end_date': datetime(),
+        # 'depends_on_past': False,
+        #'email': ['airflow@example.com'],
+        #'email_on_failure': False,
+        #'email_on_retry': False,
+        # If a task fails, retry it once after waiting
+        # at least 5 minutes
+        #'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    }
 
 with DAG(
-    dag_id='509_Kopen_Part_Daily_db2postgres_dag',
-    schedule_interval='9 5 * * *',
+    dag_id='0505_Kopen_Main_Daily_db2postgres_dag',
+    default_args=args,
+    schedule_interval='5 5 * * *',
     #start_date=datetime(year=2022, month=6, day=1),
+    #dagrun_timeout=timedelta(minutes=60),
     start_date=pendulum.datetime(2022, 6, 1, tz="Asia/Bangkok"),
     catchup=False
 ) as dag:
+    ################### DUMMY TABLE ##########################################################################################################################
+    # 1. Get the revenue type data from a table in Kopen DB2
+    task_EL_Kopen_Revenue_Type_data = PythonOperator(
+        task_id='el_kopen_revenue_type_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "BKS_REVENUE_TYPE", 'To_Table': "kp_revenue_type", 'Chunk_Size': 50000}
+    )
 
+    # 2. Get the part_monitor data from a table in Kopen DB2
+    task_EL_Kopen_Part_Monitor_data = PythonOperator(
+        task_id='el_kopen_part_monitor_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "BKS_PART_MONITOR", 'To_Table': "kp_bks_part_monitor", 'Chunk_Size': 50000}
+    )
+
+    # 3. Get the sok part data from a table in Kopen DB2
+    task_EL_Kopen_SOK_Part_data = PythonOperator(
+        task_id='el_kopen_sok_part_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "BKS_SOK_PART", 'To_Table': "kp_bks_sok_part", 'Chunk_Size': 50000}
+    )
+
+    # 4. Get the all main filter data from a table in Kopen DB2
+    task_EL_Kopen_All_Main_Filter_data = PythonOperator(
+        task_id='el_kopen_all_main_filter_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "bks_all_main_filter", 'To_Table': "kp_bks_all_main_filter", 'Chunk_Size': 50000}
+    )
+
+    # 4. Get the part demand data from a table in Kopen DB2
+    task_EL_Kopen_Part_Demand_data = PythonOperator(
+        task_id='el_kopen_part_demand_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "bks_part_demand", 'To_Table': "kp_bks_part_demand", 'Chunk_Size': 50000}
+    )
+    ################### SMALL MASTER ##########################################################################################################################
+    # 1. Get the Machine Model data from a table in Kopen DB2
+    task_EL_Kopen_Machine_Model_data = PythonOperator(
+        task_id='el_kopen_machine_model_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "unit_basic", 'To_Table': "kp_machine_model", 'Chunk_Size': 50000}
+    )
+    
+    # 2. Get the Customer data from a table in Kopen DB2
+    task_EL_Kopen_Branch_data = PythonOperator(
+        task_id='el_kopen_branch_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "branch", 'To_Table': "kp_branch", 'Chunk_Size': 50000}
+    )
+
+    # 3. Get the Customer Address data from a table in Kopen DB2
+    task_EL_Kopen_CustAddress_data = PythonOperator(
+        task_id='el_kopen_cust_address_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "customer_address", 'To_Table': "kp_customer_address", 'Chunk_Size': 50000}
+    )
+
+    # 4. Get the Part Class data from a table in Kopen DB2
+    task_EL_Kopen_Part_Class_data = PythonOperator(
+        task_id='el_kopen_part_class_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "product_class", 'To_Table': "kp_part_class", 'Chunk_Size': 50000}
+    )
+
+    # 5. Get the Department data from a table in Kopen DB2
+    task_EL_Kopen_Department_data = PythonOperator(
+        task_id='el_kopen_department_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "department", 'To_Table': "kp_department", 'Chunk_Size': 50000}
+    )
+
+    # 6. Get the Employee data from a table in Kopen DB2
+    task_EL_Kopen_Employee_data = PythonOperator(
+        task_id='el_kopen_employee_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "employee", 'To_Table': "kp_employee", 'Chunk_Size': 50000}
+    )
+
+    # 7. Get the ID data from a table in Kopen DB2
+    task_EL_Kopen_IDbooks_data = PythonOperator(
+        task_id='el_kopen_idbooks_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "idbooks", 'To_Table': "kp_idbooks", 'Chunk_Size': 50000}
+    )
+
+    # 8. Get the service code data from a table in Kopen DB2
+    task_EL_Kopen_Service_code_data = PythonOperator(
+        task_id='el_kopen_service_code_data',
+        provide_context=True,
+        python_callable=common.read_load_save_data,
+        op_kwargs={'From_Table': "SERV_BUSINESS_CODE", 'To_Table': "kp_service_code", 'Chunk_Size': 50000}
+    )
+    ################### PART ##########################################################################################################################
     # 1. Get the Part Data from a table in Kopen DB2
     task_ETL_Kopen_Part_data = PythonOperator(
         task_id='etl_kopen_part_data',
@@ -205,10 +326,10 @@ with DAG(
         python_callable= Cleansing_process,
         op_kwargs={'From_Table': "PRODUCT", 'To_Table': "kp_part", 'Chunk_Size': 50000, 'Key': 'pro_komcode', 'Condition': " and pro_lasttime >= '%s'" % (get_last_m_datetime())}
     )
-
-    branch_op = BranchPythonOperator(
+    # 5. Branch Part Data Table
+    task_Part_Branch_op = BranchPythonOperator(
         task_id="check_existing_part_on_data_warehouse",
-        python_callable=branch_func,
+        python_callable=branch_part_func,
     )
 
     branch_join = DummyOperator(
@@ -216,4 +337,10 @@ with DAG(
         trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
     )
 
-    task_ETL_Kopen_Part_data >> branch_op >> [task_L_WH_Part,task_RP_WH_Part] >> branch_join >> task_CL_WH_Part
+    task_EL_Kopen_Revenue_Type_data >> task_EL_Kopen_Part_Monitor_data >> task_EL_Kopen_SOK_Part_data \
+    >> task_EL_Kopen_All_Main_Filter_data >> task_EL_Kopen_Part_Demand_data \
+    \
+    >> task_EL_Kopen_Machine_Model_data >> task_EL_Kopen_Branch_data >> task_EL_Kopen_CustAddress_data >> task_EL_Kopen_Part_Class_data \
+    >> task_EL_Kopen_Department_data >> task_EL_Kopen_Employee_data >> task_EL_Kopen_IDbooks_data >> task_EL_Kopen_Service_code_data \
+    \
+    >> task_ETL_Kopen_Part_data >> task_Part_Branch_op >> [task_L_WH_Part,task_RP_WH_Part] >> branch_join >> task_CL_WH_Part

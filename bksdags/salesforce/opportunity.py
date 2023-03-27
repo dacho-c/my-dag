fbef6260409d93db84914c4f688a757bd10efcd7
Dacho Chuensiri <dachochuensiri@Dachos-MacBook-Pro.local>
2023-03-27 21:45:42 +0700

    Update 27 Mar 2023#2

--- bksdags/salesforce/opportunity.py
+++ bksdags/salesforce/opportunity.py
@@ -354,8 +354,6 @@ def ETL_process(**kwargs):
                 n = math.ceil(rows / 20000)
             for i in range(n):
                 r0 = i * 20000
-                if i > 0:
-                    r0 = r0 + 1
                 r1 = ((i + 1) * 20000)
                 df_1 = df.iloc[r0:r1,:]
                 df_1.to_sql(tb_to, engine, index=False, if_exists='append')

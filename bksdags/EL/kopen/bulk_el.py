ff01768c2025092163e9663ecf323bee36d549ea
Dacho Chuensiri <dachochuensiri@BKSIT-MacBook-Pro.local>
2023-07-23 20:09:41 +0700

    Update 23 July 2023#8

--- bksdags/EL/kopen/bulk_el.py
+++ bksdags/EL/kopen/bulk_el.py
@@ -74,7 +74,7 @@ with DAG(
                 }),
             headers={"accept": "application/json"},
         )
-        time_wait = TimeDeltaSensor(task_id=f"wait_for_export_{i}", delta=timedelta(seconds=set_delay(i))
+        time_wait = TimeDeltaSensor(task_id=f"wait_for_export_{i}", delta=timedelta(seconds=set_delay(i)))
         check_process = PythonOperator(
             task_id=f"check_{i}",
             trigger_rule=TriggerRule.ALL_DONE,
